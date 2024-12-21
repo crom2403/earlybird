51b5194aa6d6d47a36cd35e4c77442f93a29e13b
tsondev <thanhsonrau2002@gmail.com>
2024-12-21 11:31:50 +0700

    add modal done learn

--- src/components/vocabulary/VocabularyEditor.tsx
+++ src/components/vocabulary/VocabularyEditor.tsx
@@ -1,253 +1,3 @@
-// "use client"
-
-// import React, { useEffect, useState } from "react"
-// import Editor from "react-simple-code-editor"
-// import { highlight, languages } from "prismjs"
-// import "prismjs/themes/prism.css"
-// import { InputVocabulary } from "@/types/vocabulary"
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select"
-// import { cn } from "@/lib/utils"
-
-// interface VocabularyState {
-//   attempts: number
-//   isCorrect: boolean
-// }
-
-// const VocabularyEditor = ({ listVocabulary }: { listVocabulary: InputVocabulary[] | [] }) => {
-//   const [code, setCode] = useState("")
-//   const [theme, setTheme] = useState("dark")
-//   const [counter, setCounter] = useState<number>(0)
-//   const [mode, setMode] = useState<string>("learn")
-//   const [vocabularyStats, setVocabularyStats] = useState<Map<string, VocabularyState>>(new Map())
-//   const color = {
-//     dark: "bg-gradient-to-br from-[#0f172a] to-[#070b17] text-white",
-//     light: "bg-white text-black",
-//     pink: "bg-gradient-to-br from-[#fa87ad] to-[#f76897] text-white",
-//     green: "bg-[#0d301f] text-[#fedb3f]",
-//     gradient_1: "bg-gradient-to-br from-pink-600 to-violet-600 text-white",
-//     gradient_2: "bg-gradient-to-br from-orange-600 to-blue-600 text-white",
-//     gradient_3: "bg-gradient-to-br from-teal-400 to-blue-500 text-white", // Gradient xanh lam
-//     gradient_4: "bg-gradient-to-br from-purple-500 to-red-500 text-white", // Gradient tím và đỏ
-//     gradient_5: "bg-gradient-to-br from-yellow-400 to-red-500 text-white", // Gradient vàng và đỏ
-//     gradient_6: "bg-gradient-to-br from-green-400 to-blue-500 text-white", // Gradient xanh lá và xanh lam
-//     gradient_7: "bg-gradient-to-br from-indigo-500 to-pink-500 text-white", // Gradient xanh dương và hồng
-//     gradient_8: "bg-gradient-to-br from-gray-700 to-gray-900 text-white", // Gradient xám
-//     gradient_9: "bg-gradient-to-br from-red-400 to-yellow-500 text-white", // Gradient đỏ và vàng
-//     gradient_10: "bg-gradient-to-br from-blue-400 to-purple-600 text-white", // Gradient xanh lam và tím
-//   }
-
-//   const currentVocab = listVocabulary[counter]
-
-//   useEffect(() => {
-//     resetEditor()
-//   }, [counter, mode])
-
-//   const resetEditor = () => {
-//     const terminology = currentVocab.terminology
-//     setCode(`Thuật ngữ: ${terminology}\nĐịnh nghĩa là: `)
-//   }
-
-//   const getVocabStats = (terminology: string): VocabularyState => {
-//     return (
-//       vocabularyStats.get(terminology) || {
-//         attempts: 0,
-//         isCorrect: false,
-//       }
-//     )
-//   }
-
-//   const updateVocabStats = (terminology: string, isCorrect: boolean) => {
-//     const stats = getVocabStats(terminology)
-//     const newStats = {
-//       attempts: stats.attempts + 1,
-//       isCorrect: isCorrect,
-//     }
-//     setVocabularyStats(new Map(vocabularyStats.set(terminology, newStats)))
-//   }
-
-//   const handleCodeChange = (newCode: string) => {
-//     const prefix = `Thuật ngữ: ${currentVocab.terminology}\nĐịnh nghĩa là: `
-//     const prefixWithCorrect = `Thuật ngữ: ${currentVocab.terminology}\nĐịnh nghĩa đúng là: ${currentVocab.define}\nĐịnh nghĩa là: `
-
-//     if (!newCode.startsWith(prefix) && !newCode.startsWith(prefixWithCorrect)) {
-//       return
-//     }
-//     setCode(newCode)
-//   }
-
-//   const checkAnswer = (userAnswer: string): boolean => {
-//     const normalizedAnswer = userAnswer.trim().toLowerCase()
-//     const normalizedCorrect = currentVocab.define.trim().toLowerCase()
-//     return normalizedAnswer === normalizedCorrect
-//   }
-
-//   const handleKeyDown = (event: React.KeyboardEvent) => {
-//     if (event.key !== "Enter") return
-//     event.preventDefault()
-
-//     const userAnswer =
-//       mode === "learn" ? code.split("Định nghĩa là: ")[1] : code.split("Định nghĩa là: ")[1]
-
-//     const isCorrect = checkAnswer(userAnswer)
-//     updateVocabStats(currentVocab.terminology, isCorrect)
-
-//     if (isCorrect) {
-//       if (counter < listVocabulary.length - 1) {
-//         setCounter(counter + 1)
-//       } else {
-//         setCode("Chúc mừng! Bạn đã hoàn thành tất cả các từ vựng!")
-//       }
-//     } else {
-//       const feedbackMessage =
-//         mode === "learn"
-//           ? `Thuật ngữ: ${currentVocab.terminology}\nĐịnh nghĩa đúng là: ${currentVocab.define}\nĐịnh nghĩa là: ${userAnswer}`
-//           : `Câu trả lời chưa chính xác. Hãy thử lại!\nThuật ngữ: ${currentVocab.terminology}\nĐịnh nghĩa là: `
-
-//       setCode(feedbackMessage)
-//     }
-//   }
-
-//   const lineNumbers = (code: string) => {
-//     return code
-//       .split("\n")
-//       .map((_, i) => i + 1)
-//       .join("\n")
-//   }
-
-//   return (
-//     <div className="w-full flex items-center justify-center">
-//       <div className={`flex w-full flex-col gap-4 m-4 max-w-[600px]`}>
-//         <div className="flex w-full flex-col gap-2">
-//           <div
-//             className={cn(
-//               "flex items-center gap-2 p-2 rounded-md",
-//               theme === "dark" ? color.dark : "",
-//               theme === "light" ? color.light : "",
-//               theme === "green" ? color.green : "",
-//               theme === "pink" ? color.pink : "",
-//               theme === "gradient1" ? color.gradient_1 : "",
-//               theme === "gradient2" ? color.gradient_2 : "",
-//               theme === "gradient3" ? color.gradient_3 : "",
-//               theme === "gradient4" ? color.gradient_4 : "",
-//               theme === "gradient5" ? color.gradient_5 : "",
-//               theme === "gradient6" ? color.gradient_6 : "",
-//               theme === "gradient7" ? color.gradient_7 : "",
-//               theme === "gradient8" ? color.gradient_8 : "",
-//               theme === "gradient9" ? color.gradient_9 : "",
-//               theme === "gradient10" ? color.gradient_10 : ""
-//             )}
-//           >
-//             <Select value={mode} onValueChange={setMode}>
-//               <SelectTrigger
-//                 className={`w-[90px] border-none ring-0 outline-none ${
-//                   theme === "dark" ? "bg-[#060b17] text-white" : "bg-[#e8e9eb] text-black"
-//                 }`}
-//               >
-//                 <SelectValue placeholder="Chế độ học" />
-//               </SelectTrigger>
-//               <SelectContent className="w-fit">
-//                 <SelectItem value="learn">Learn</SelectItem>
-//                 <SelectItem value="test">Test</SelectItem>
-//               </SelectContent>
-//             </Select>
-//             <Select value={theme} onValueChange={setTheme}>
-//               <SelectTrigger
-//                 className={`min-w-[90px] max-w-[110px] border-none ring-0 outline-none ${
-//                   theme === "dark" ? "bg-[#060b17] text-white" : "bg-[#e8e9eb] text-black"
-//                 }`}
-//               >
-//                 <SelectValue placeholder="Theme" />
-//               </SelectTrigger>
-//               <SelectContent className="w-fit">
-//                 <SelectItem value="light">Light</SelectItem>
-//                 <SelectItem value="dark">Dark</SelectItem>
-//                 <SelectItem value="green">Green</SelectItem>
-//                 <SelectItem value="pink">Pink</SelectItem>
-//                 <SelectItem value="gradient1">Gradient 1</SelectItem>
-//                 <SelectItem value="gradient2">Gradient 2</SelectItem>
-//                 <SelectItem value="gradient3">Gradient 3</SelectItem>
-//                 <SelectItem value="gradient4">Gradient 4</SelectItem>
-//                 <SelectItem value="gradient5">Gradient 5</SelectItem>
-//                 <SelectItem value="gradient6">Gradient 6</SelectItem>
-//                 <SelectItem value="gradient7">Gradient 7</SelectItem>
-//                 <SelectItem value="gradient8">Gradient 8</SelectItem>
-//                 <SelectItem value="gradient9">Gradient 9</SelectItem>
-//                 <SelectItem value="gradient10">Gradient 10</SelectItem>
-//               </SelectContent>
-//             </Select>
-//           </div>
-//         </div>
-//         <div
-//           className={cn(
-//             "w-full flex flex-col border shadow-md rounded-md overflow-hidden border-none p-2 text-white",
-//             theme === "dark" ? color.dark : "",
-//             theme === "light" ? color.light : "",
-//             theme === "green" ? color.green : "",
-//             theme === "pink" ? color.pink : "",
-//             theme === "gradient1" ? color.gradient_1 : "",
-//             theme === "gradient2" ? color.gradient_2 : "",
-//             theme === "gradient3" ? color.gradient_3 : "",
-//             theme === "gradient4" ? color.gradient_4 : "",
-//             theme === "gradient5" ? color.gradient_5 : "",
-//             theme === "gradient6" ? color.gradient_6 : "",
-//             theme === "gradient7" ? color.gradient_7 : "",
-//             theme === "gradient8" ? color.gradient_8 : "",
-//             theme === "gradient9" ? color.gradient_9 : "",
-//             theme === "gradient10" ? color.gradient_10 : ""
-//           )}
-//         >
-//           <div className="flex items-center justify-between">
-//             <div className={`w-full h-8 flex gap-2 items-center px-2 bg-transparent`}>
-//               <div className="size-4 rounded-full bg-red-600"></div>
-//               <div className="size-4 rounded-full bg-yellow-600"></div>
-//               <div className="size-4 rounded-full bg-green-600"></div>
-//             </div>
-//           </div>
-//           <div className="flex fireworks-container relative">
-//             <div
-//               className={`text-[#696c6e] text-right px-2 py-[10px] select-none`}
-//               style={{
-//                 fontFamily: "monospace",
-//                 fontSize: "16px",
-//                 lineHeight: "1.6",
-//                 whiteSpace: "pre",
-//               }}
-//             >
-//               {lineNumbers(code)}
-//             </div>
-//             <div className="flex-1 border-none">
-//               <Editor
-//                 value={code}
-//                 onValueChange={handleCodeChange}
-//                 onKeyDown={handleKeyDown}
-//                 highlight={(code) => highlight(code, languages.text, "text")}
-//                 padding={10}
-//                 style={{
-//                   fontFamily: "monospace",
-//                   fontSize: "16px",
-//                   lineHeight: "1.6",
-//                   minHeight: "300px",
-//                   outline: "none",
-//                   border: "none",
-//                 }}
-//                 className="text-lg focus:outline-none focus:border-none ring-0 focus:ring-0 "
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default VocabularyEditor
-
 "use client"
 
 import React, { useEffect, useState } from "react"
@@ -256,6 +6,7 @@ import { highlight, languages } from "prismjs"
 import "prismjs/themes/prism.css"
 import { InputVocabulary } from "@/types/vocabulary"
 import Link from "next/link"
+import { BorderBeam } from "@/components/ui/border-beam"
 import {
   Select,
   SelectContent,
@@ -263,13 +14,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogFooter,
-} from "@/components/ui/dialog"
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogFooter,
+// } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
@@ -474,7 +225,7 @@ const VocabularyEditor = ({ listVocabulary }: { listVocabulary: InputVocabulary[
         <div className="flex w-full flex-col gap-2">
           <div
             className={cn(
-              "flex items-center gap-2 p-2 rounded-md",
+              "flex items-center gap-2 p-2 rounded-md shadow-md",
               color[theme as keyof typeof color]
             )}
           >
@@ -594,32 +345,39 @@ const VocabularyEditor = ({ listVocabulary }: { listVocabulary: InputVocabulary[
           </div>
         </div>
 
-        <Dialog open={showResults} onOpenChange={setShowResults}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Kết quả học tập</DialogTitle>
-            </DialogHeader>
-            <div className="space-y-4">
-              <p>Tổng số từ: {listVocabulary.length}</p>
-              {mode === "test" && (
-                <>
-                  <p>Số câu đúng: {stats.correctAnswers}</p>
-                  <p>Số câu sai: {stats.wrongAnswers}</p>
-                  <p>
-                    Tỷ lệ đúng: {((stats.correctAnswers / listVocabulary.length) * 100).toFixed(1)}%
-                  </p>
-                </>
-              )}
-              <p>Thời gian hoàn thành: {formatTime(stats.totalTime)}</p>
+        {showResults && (
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="bg-white relative max-w-[600px] flex h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border md:shadow-xl"
+            >
+              <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-3xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+                Kết quả học tập
+              </span>
+              <div className="space-y-4">
+                <p>Tổng số từ: {listVocabulary.length}</p>
+                {mode === "test" && (
+                  <>
+                    <p>Số câu đúng: {stats.correctAnswers}</p>
+                    <p>Số câu sai: {stats.wrongAnswers}</p>
+                    <p>
+                      Tỷ lệ đúng:{" "}
+                      {((stats.correctAnswers / listVocabulary.length) * 100).toFixed(1)}%
+                    </p>
+                  </>
+                )}
+                <p>Thời gian hoàn thành: {formatTime(stats.totalTime)}</p>
+              </div>
+              <div className="flex gap-2 mt-4">
+                <Button onClick={resetLearning}>Học lại</Button>
+                <Link href="/vocabulary">
+                  <Button variant="outline">Trở về</Button>
+                </Link>
+              </div>
+              <BorderBeam size={250} duration={12} delay={9} />
             </div>
-            <DialogFooter className="flex gap-2">
-              <Button onClick={resetLearning}>Học lại</Button>
-              <Link href="/vocabulary">
-                <Button variant="outline">Trở về</Button>
-              </Link>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+          </div>
+        )}
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,115 +14,121 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        dark: {
-          main_background: "#121212",
-          main_foreground: "#ffffff",
-        },
-        light: {
-          main_background: "#ffffff",
-          main_foreground: "#000000",
-        },
-        board: {
-          blue: {
-            border: "#0e538c",
-            text: "#0e538c",
-            background: "#cbe4ff",
-            bg_button: "#d9ebff",
-          },
-          green: {
-            border: "#2e7d32", // Màu xanh lá cây đậm
-            text: "#2e7d32", // Màu xanh lá cây đậm cho văn bản
-            background: "#c8e6c9", // Màu nền xanh lá cây nhạt
-            bg_button: "#e0f2f1", // Màu nền nút xanh lá cây nhạt hơn
-          },
-          orange: {
-            border: "#d5710d",
-            text: "#d5710d",
-            background: "#fae6cb",
-            bg_button: "#f9f5e9",
-          },
-          violet: {
-            border: "#6a1b9a", // Màu violet đậm
-            text: "#6a1b9a", // Màu violet đậm cho văn bản
-            background: "#e1bee7", // Màu nền violet nhạt
-            bg_button: "#f3e5f5", // Màu nền nút violet nhạt hơn
-          },
-          pink: {
-            border: "#d81b60", // Màu hồng đậm
-            text: "#d81b60", // Màu hồng đậm cho văn bản
-            background: "#f8bbd0", // Màu nền hồng nhạt
-            bg_button: "#fce4e1", // Màu nền nút hồng nhạt hơn
-          },
-          red: {
-            border: "#c62828", // Màu đỏ đậm
-            text: "#c62828", // Màu đỏ đậm cho văn bản
-            background: "#ef9a9a", // Màu nền đỏ nhạt
-            bg_button: "#f2b2b2", // Màu nền nút đỏ nhạt hơn
-          },
-          mint: {
-            border: "#4db6ac", // Màu mint đậm
-            text: "#4db6ac", // Màu mint đậm cho văn bản
-            background: "#b2dfdb", // Màu nền mint nhạt
-            bg_button: "#e0f2f1", // Màu nền nút mint nhạt hơn
-          },
-        },
-        sidebar: {
-          DEFAULT: "hsl(var(--sidebar-background))",
-          foreground: "hsl(var(--sidebar-foreground))",
-          primary: "hsl(var(--sidebar-primary))",
-          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
-          accent: "hsl(var(--sidebar-accent))",
-          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
-          border: "hsl(var(--sidebar-border))",
-          ring: "hsl(var(--sidebar-ring))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      animation: {
-        aurora: "aurora 60s linear infinite",
-        spotlight: "spotlight 2s ease .75s 1 forwards",
-        shimmer: "shimmer 2s linear infinite",
-        "spin-reverse": "spin 1s linear infinite reverse",
-        "spin-slow": "spin 3s linear infinite",
-        "spin-reverse-slow": "spin 3s linear infinite reverse",
-      },
-      keyframes: {
-        shimmer: {
-          from: {
-            backgroundPosition: "0 0",
-          },
-          to: {
-            backgroundPosition: "-200% 0",
-          },
-        },
-        spotlight: {
-          "0%": {
-            opacity: "0",
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: "1",
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			dark: {
+  				main_background: '#121212',
+  				main_foreground: '#ffffff'
+  			},
+  			light: {
+  				main_background: '#ffffff',
+  				main_foreground: '#000000'
+  			},
+  			board: {
+  				blue: {
+  					border: '#0e538c',
+  					text: '#0e538c',
+  					background: '#cbe4ff',
+  					bg_button: '#d9ebff'
+  				},
+  				green: {
+  					border: '#2e7d32',
+  					text: '#2e7d32',
+  					background: '#c8e6c9',
+  					bg_button: '#e0f2f1'
+  				},
+  				orange: {
+  					border: '#d5710d',
+  					text: '#d5710d',
+  					background: '#fae6cb',
+  					bg_button: '#f9f5e9'
+  				},
+  				violet: {
+  					border: '#6a1b9a',
+  					text: '#6a1b9a',
+  					background: '#e1bee7',
+  					bg_button: '#f3e5f5'
+  				},
+  				pink: {
+  					border: '#d81b60',
+  					text: '#d81b60',
+  					background: '#f8bbd0',
+  					bg_button: '#fce4e1'
+  				},
+  				red: {
+  					border: '#c62828',
+  					text: '#c62828',
+  					background: '#ef9a9a',
+  					bg_button: '#f2b2b2'
+  				},
+  				mint: {
+  					border: '#4db6ac',
+  					text: '#4db6ac',
+  					background: '#b2dfdb',
+  					bg_button: '#e0f2f1'
+  				}
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			aurora: 'aurora 60s linear infinite',
+  			spotlight: 'spotlight 2s ease .75s 1 forwards',
+  			shimmer: 'shimmer 2s linear infinite',
+  			'spin-reverse': 'spin 1s linear infinite reverse',
+  			'spin-slow': 'spin 3s linear infinite',
+  			'spin-reverse-slow': 'spin 3s linear infinite reverse',
+  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear'
+  		},
+  		keyframes: {
+  			shimmer: {
+  				from: {
+  					backgroundPosition: '0 0'
+  				},
+  				to: {
+  					backgroundPosition: '-200% 0'
+  				}
+  			},
+  			spotlight: {
+  				'0%': {
+  					opacity: '0',
+  					transform: 'translate(-72%, -62%) scale(0.5)'
+  				},
+  				'100%': {
+  					opacity: '1',
+  					transform: 'translate(-50%,-40%) scale(1)'
+  				}
+  			},
+  			aurora: {
+  				from: {
+  					backgroundPosition: '50% 50%, 50% 50%'
+  				},
+  				to: {
+  					backgroundPosition: '350% 50%, 350% 50%'
+  				}
+  			},
+  			'border-beam': {
+  				'100%': {
+  					'offset-distance': '100%'
+  				}
+  			}
+  		}
+  	}
   },
   plugins: [
     require("tailwindcss-animate"),
