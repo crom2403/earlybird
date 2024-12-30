15971cf9bdc228ca030a91afbf20dc65b35b9951
tsondev <thanhsonrau2002@gmail.com>
2024-12-30 12:02:16 +0700

    change default dark mode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
       {children}
     </ThemeProvider>
   )
